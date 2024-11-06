bb1d588fbbd36f1323a865d6b7b7fd121f878e40
fntnppp <tanaphon_p@cmu.ac.th>
2024-11-06 21:08:45 +0700

    edit-f-reply

--- venv/app/views.py
+++ venv/app/views.py
@@ -84,25 +84,7 @@ def reply(intent,text,reply_token,id,disname):
         text_message = TextSendMessage(text='ทดสอบสำเร็จ')
         line_bot_api.reply_message(reply_token,text_message)
         
-    if intent == 'rose':
-        image_links = [
-            {'original': 'https://i.pinimg.com/564x/06/fd/71/06fd71d931657acfd6dc128c914842d4.jpg', 'preview': 'https://i.pinimg.com/564x/06/fd/71/06fd71d931657acfd6dc128c914842d4.jpg'},
-            {'original': 'https://i.pinimg.com/564x/c0/c7/7c/c0c77c79f82a14dc9107b6602d6aef1f.jpg', 'preview': 'https://i.pinimg.com/564x/c0/c7/7c/c0c77c79f82a14dc9107b6602d6aef1f.jpg'},
-            {'original': 'https://i.pinimg.com/564x/95/0f/4f/950f4f90ff334ad7e645084edc9f89be.jpg', 'preview': 'https://i.pinimg.com/564x/95/0f/4f/950f4f90ff334ad7e645084edc9f89be.jpg'},
-            {'original': 'https://i.pinimg.com/564x/d2/b7/ad/d2b7adc3478744852d83e681b0f50040.jpg', 'preview': 'https://i.pinimg.com/564x/d2/b7/ad/d2b7adc3478744852d83e681b0f50040.jpg'},
-            {'original': 'https://i.pinimg.com/736x/56/ca/e3/56cae392dbbf1f1e27789a457193d4cd.jpg', 'preview': 'https://i.pinimg.com/736x/56/ca/e3/56cae392dbbf1f1e27789a457193d4cd.jpg'},
-            {'original': 'https://i.pinimg.com/564x/34/81/a7/3481a7bd322085a3815651e03ec3a721.jpg', 'preview': 'https://i.pinimg.com/564x/34/81/a7/3481a7bd322085a3815651e03ec3a721.jpg'},
-            {'original': 'https://i.pinimg.com/564x/4f/34/d2/4f34d2dad8546b06aeb42f8e067ef733.jpg', 'preview': 'https://i.pinimg.com/564x/4f/34/d2/4f34d2dad8546b06aeb42f8e067ef733.jpg'},
-            {'original': 'https://i.pinimg.com/564x/2e/88/ca/2e88ca4472d3c97ff5802f9dbb20a8c8.jpg', 'preview': 'https://i.pinimg.com/564x/2e/88/ca/2e88ca4472d3c97ff5802f9dbb20a8c8.jpg'},
-            {'original': 'https://i.pinimg.com/564x/a4/15/04/a4150473d1bed9954a5ffdf196d19e8a.jpg', 'preview': 'https://i.pinimg.com/564x/a4/15/04/a4150473d1bed9954a5ffdf196d19e8a.jpg'}
-        ]
-
-        chosen_image = random.choice(image_links)        
-        image_message = ImageSendMessage(
-            original_content_url=chosen_image['original'],
-            preview_image_url=chosen_image['preview']
-        )
-        line_bot_api.reply_message(reply_token, image_message)
+    
         
 
 if __name__ == '__main__':
